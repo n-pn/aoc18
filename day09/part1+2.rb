d53e38e7346d996dd678226025f6fd835a608616
nipinium <nipinium@gmail.com>
2018-12-09 13:27:05 +0700

    Remove functions

--- day09/part1+2.rb
+++ day09/part1+2.rb
@@ -1,48 +1,37 @@
 Node = Struct.new(:prev, :next)
 
-@ring = {}
+max = 7158800
+len = 430
+idx = 0
+arr = Array.new(len, 0)
+
+@ring = Array.new(max + 1)
 @ring[0] = Node.new(0, 0)
 @curr = 0
 
-def insert(idx)
-  _prev = @ring[@curr].next
-  _next = @ring[_prev].next
+(1..max).each do |curr|
+  idx = (idx + 1) % len
 
-  @ring[_prev].next = idx
-  @ring[_next].prev = idx
-  @ring[idx] = Node.new(_prev, _next)
-  @curr = idx
-end
+  if curr % 23 == 0
+    7.times { @curr = @ring[@curr].prev }
 
-def remove
-  7.times { @curr = @ring[@curr].prev }
+    _next = @ring[@curr].next
+    _prev = @ring[@curr].prev
 
-  _next = @ring[@curr].next
-  _prev = @ring[@curr].prev
+    @ring[_next].prev = _prev
+    @ring[_prev].next = _next
 
-  @ring[_next].prev = _prev
-  @ring[_prev].next = _next
-
-  res = @curr
-  @curr = _next
-
-  res
-end
-
-m = 7158800
-n = 430
-i = 0
-a = Array.new(n, 0)
-
-m.times do |x|
-  x += 1
-  i = (i + 1) % n
-
-  if x % 23 == 0
-    a[i] += x + remove()
+    arr[idx] += curr + @curr
+    @curr = _next
   else
-    insert(x)
+    _prev = @ring[@curr].next
+    _next = @ring[_prev].next
+
+    @ring[_prev].next = curr
+    @ring[_next].prev = curr
+    @ring[curr] = Node.new(_prev, _next)
+    @curr = curr
   end
 end
 
-puts a.max
+puts arr.max
