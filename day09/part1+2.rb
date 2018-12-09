d4ba84722f6de27ef55a73c1fc13bac953fc0040
nipinium <nipinium@gmail.com>
2018-12-09 13:43:31 +0700

    Split @ring to @prev and @next

--- day09/part1+2.rb
+++ day09/part1+2.rb
@@ -1,35 +1,36 @@
-Node = Struct.new(:prev, :next)
-
 max = 7158800
 len = 430
 idx = 0
 arr = Array.new(len, 0)
 
-@ring = Array.new(max + 1)
-@ring[0] = Node.new(0, 0)
+@prev = Array.new(max + 1)
+@next = Array.new(max + 1)
+@prev[0] = @next[0] = 0
 @curr = 0
 
 (1..max).each do |curr|
   idx = (idx + 1) % len
 
   if curr % 23 == 0
-    7.times { @curr = @ring[@curr].prev }
+    7.times { @curr = @prev[@curr] }
 
-    _next = @ring[@curr].next
-    _prev = @ring[@curr].prev
+    _next = @next[@curr]
+    _prev = @prev[@curr]
 
-    @ring[_next].prev = _prev
-    @ring[_prev].next = _next
+    @prev[_next] = _prev
+    @next[_prev] = _next
 
     arr[idx] += curr + @curr
     @curr = _next
   else
-    _prev = @ring[@curr].next
-    _next = @ring[_prev].next
+    _prev = @next[@curr]
+    _next = @next[_prev]
 
-    @ring[_prev].next = curr
-    @ring[_next].prev = curr
-    @ring[curr] = Node.new(_prev, _next)
+    @next[_prev] = curr
+    @prev[_next] = curr
+    @prev[curr] = _prev
+    @next[curr] = _next
+
     @curr = curr
   end
 end
